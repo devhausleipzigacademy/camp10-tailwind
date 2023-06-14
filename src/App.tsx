b3406883568c34d373f99d1e1fc945330ef9e8e8
Julian Vogel <birdman.coding.1991@gmail.com>
2023-06-14 16:28:57 +0200

    blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import BlogCard from "./components/card/BlogCard";
 import Navbar from "./components/navbar/Navbar";
 
+export type BlogPost = {
+  id: number;
+  title: string;
+  author: string;
+  img: string;
+  date: string;
+  content: string;
+};
+
 function App() {
+  const [blogPosts, setBlogPosts] = useState<BlogPost[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/posts")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: BlogPost[]) => {
+        setBlogPosts(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <header>
@@ -15,8 +40,18 @@ function App() {
           molestias necessitatibus officia iusto veniam quidem odio dolore
           praesentium ut, aspernatur cumque laboriosam!
         </div>
-        <div className="bg-neutral-100 mt-10 h-screen p-6">
-          <BlogCard />
+        <div className="bg-neutral-100 mt-10 flex gap-5 justify-center p-6 ">
+          {blogPosts.map((post) => (
+            <BlogCard
+              author={post.author}
+              content={post.content}
+              title={post.title}
+              date={post.date}
+              img={post.img}
+              id={post.id}
+              key={post.id}
+            />
+          ))}
         </div>
       </main>
     </>
--- src/components/card/BlogCard.tsx
+++ src/components/card/BlogCard.tsx
@@ -1,24 +1,19 @@
-function BlogCard() {
+import { BlogPost } from "../../App";
+
+interface Props extends BlogPost {}
+
+function BlogCard({ author, content, date, img, title, id }: Props) {
   return (
     <div className="w-[350px] max-h-[600px] bg-white rounded-md overflow-hidden shadow-md cursor-pointer transition ease-in-out duration-300  hover:scale-105">
       <div className="relative">
-        <img
-          src="https://source.unsplash.com/featured/300x201"
-          alt=""
-          className="w-full"
-        />
+        <img src={img} alt="" className="w-full" />
         <time className="absolute left-0 top-6 bg-secondary text-white py-2 px-6 shadow-sm rounded-r-md">
-          12.08.2023
+          {date}
         </time>
       </div>
       <div className="px-4 py-8">
-        <h3 className="text-slate-700 text-2xl mb-3">Some Title</h3>
-        <p className="text-slate-500">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Voluptatibus, excepturi, illo optio impedit accusantium quidem
-          commodi, ullam ab laudantium harum doloremque beatae ratione
-          voluptatem vero adipisci. Libero harum perspiciatis at.
-        </p>
+        <h3 className="text-slate-700 text-2xl mb-3">{title}</h3>
+        <p className="text-slate-500">{content}</p>
       </div>
     </div>
   );
