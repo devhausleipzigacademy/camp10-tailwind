5f1709d30ed1c70ed392d5d7490222a7abbc8ad0
Julian Vogel <birdman.coding.1991@gmail.com>
2023-06-14 15:50:27 +0200

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import BlogCard from "./components/card/BlogCard";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
@@ -6,10 +7,17 @@ function App() {
       <header>
         <Navbar />
       </header>
-      <main className="container m-auto">
-        <h1 className="heading-font">Welcome to the Blog</h1>
-
-        <h2 className="heading-font">Welcome to the Blog</h2>
+      <main>
+        <h1 className="heading-font px-3">Welcome to the Blog</h1>
+        <div className="text-center text-slate-500 max-w-3xl m-auto  px-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iste
+          laudantium fuga cupiditate, explicabo minus consequuntur. Distinctio
+          molestias necessitatibus officia iusto veniam quidem odio dolore
+          praesentium ut, aspernatur cumque laboriosam!
+        </div>
+        <div className="bg-neutral-100 mt-10 h-screen p-6">
+          <BlogCard />
+        </div>
       </main>
     </>
   );
---
+++ src/components/card/BlogCard.tsx
@@ -0,0 +1,30 @@
+function BlogCard() {
+  return (
+    <div className="w-[350px] max-h-[600px] bg-white rounded-md overflow-hidden shadow-md">
+      <div className="relative">
+        <img
+          src="https://source.unsplash.com/featured/300x201"
+          alt=""
+          className="w-full"
+        />
+        <time className="absolute left-0 top-6 bg-secondary text-white py-2 px-6 shadow-sm rounded-r-md">
+          12.08.2023
+        </time>
+      </div>
+      <div className="px-4 py-8">
+        <h3 className="text-slate-700 text-2xl mb-3">Some Title</h3>
+        <p className="text-slate-500">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatibus, excepturi, illo optio impedit accusantium quidem
+          commodi, ullam ab laudantium harum doloremque beatae ratione
+          voluptatem vero adipisci. Libero harum perspiciatis at.
+        </p>
+        <button className="bg-primary px-6 py-2 rounded-lg text-white shadow-sm mt-4">
+          read more...
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
